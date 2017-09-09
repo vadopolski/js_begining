1765e0d5a3fc8e8cf2df5fcaa63762b464564dbb
vadopolski <vop444@gmail.com>
2017-09-09 17:04:26 +0300

    first day commit

---
+++ case_construction.js
@@ -0,0 +1,18 @@
+num = 11;
+
+switch (num) {
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3 :
+        console.log("Threee");
+        break;
+    default:
+        console.log("More more");
+}
--- lab_3.js
+++ lab_3.js
@@ -1,7 +1,7 @@
-const AGE = 33;
+const AGE = -20;
 let output = "Неизвестный возраст";
 
-if (AGE <= 17)
+if (AGE <= 17 && AGE > 0)
     output = "Вам работать еще рано - учитесь.";
 else if (AGE > 17 && AGE <= 59)
     output = "Вам еще работать и работать.";
