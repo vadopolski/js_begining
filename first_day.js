8db2e7a0b7f28e899781697863a94e4a57b2f542
vadopolski <vop444@gmail.com>
2017-09-09 16:32:39 +0300

    Initial commit

---
+++ first_day.js
@@ -0,0 +1,10 @@
+// if else construction
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0)
+        console.log(i + " число чётное");
+    else if (i == 0)
+        console.log(i + " это ноль")
+    else
+        console.log(i + " число нечётное")
+}
+
---
+++ lab_3.js
@@ -0,0 +1,8 @@
+const AGE = 44;
+
+if (AGE <= 17)
+    console.log("Вам работать еще рано - учитесь.")
+else if (AGE > 17 && AGE <= 59)
+    console.log("Вам еще работать и работать.")
+else (AGE > 59)
+    console.log("Вам пора на пенсию.")
