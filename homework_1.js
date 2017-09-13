81076b31ba02a3503f31879d0ec2ba702fd21451
vadopolski <vop444@gmail.com>
2017-09-13 22:56:13 +0300

    Added second homework.

--- homework_1.js
+++ homework_1.js
@@ -1,3 +1,19 @@
 // вороны
 
 // елочка
+let res2 = "*";
+
+for (let i = 0; i < 5; i++) {
+    let i1 = i;
+    let res1 = "";
+
+    while (i1 < 5) {
+        res1 += " ";
+        i1++;
+    }
+
+    res2 += "**";
+
+    console.log(res1 + res2 + res1);
+
+}
